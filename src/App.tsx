79c177a6e362277feb453123b2500f08db0a09e6
Abdugaffarov2002 <pulat.96kg@gmail.com>
2023-07-19 18:34:36 +0600

    react-intro

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import Input from "./components/Input";
+
+function App() {
+  /// JS world
+
+  return (
+    /// JSX world
+    <div>
+      <Counter />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
